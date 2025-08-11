f3ad6f426a62bf07823034ae988b5b669c0851f4
york-marcos <MandradeC@yorksolutions.net>
2025-08-11 14:33:22 -0500

    chore: cleanup unused files

--- DTOs/Response/StatusTransitionResponse.cs
+++
@@ -1,6 +0,0 @@
-namespace TSG_Commex_BE.DTOs.Response;
-
-public class StatusTransitionResponse
-{
-    // TODO: Add properties for status transition data
-}
--- DTOs/Response/WorkflowResponse.cs
+++
@@ -1,6 +0,0 @@
-namespace TSG_Commex_BE.DTOs.Response;
-
-public class WorkflowResponse
-{
-    // TODO: Add properties for workflow data sent to admin UI
-}
